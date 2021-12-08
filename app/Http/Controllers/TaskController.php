8dd8222ca69606ef31b0f7c8e90510725bb247ce
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-12-08 10:24:59 +0800

    建構布局與視圖

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -15,5 +15,8 @@ class TaskController extends Controller
     {
         $this->middleware('auth');
     }
-
+    public function index(Request $request)
+    {
+        return view('tasks.index');
+    }
 }
