f1241bfa0e22d76b1e84de3648c123a55f2a528e
3A832094 <3a832094@gm.student.ncut.edu.tw>
2021-12-08 10:20:03 +0800

    建立任務控制器

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Controller;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+Route::post('/task', [\App\Http\Controllers\TaskController::class,'store'])->name('tasks.store');
+Route::delete('/task/{task}', [\App\Http\Controllers\TaskController::class,'destroy'])->name('tasks.destroy');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
